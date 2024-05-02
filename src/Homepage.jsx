e429419f607bf780d7a128bc6c41f96906859adc
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-02 19:58:06 +0000

    Homepage modified

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="flex flex-col justify-center m-auto">
+      <h1 className="text-2xl font-bold">Welcome to the Homepage</h1>
+
+      <div className="flex justify-center gap-5 mt-8">
+        <button>
+          <Link to={"/login"}>login</Link>
+        </button>
+        <button>
+          <Link to={"/register"}>Register</Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
