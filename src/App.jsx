4535d9d849fc73c406262a1c65df6590c018c30f
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-02 01:47:11 +0000

    Added Registration & Submission Logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Register from "./Register";
+
 function App() {
-  return <></>;
+  return (
+    <div className="bg-[#135D66] p-6 w-full text-white m-auto max-w-[420px] min-h-[400px] flex justify-center">
+      <Register />
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <main className="bg-[#E3FEF7] w-full min-h-screen flex justify-center">
+      <App />
+    </main>
+  </React.StrictMode>
+);
